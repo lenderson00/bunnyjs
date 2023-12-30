d46049b0325aedfc553102a1a05c1fce89f850bd
Lenderson Macedo <61628075+lenderson00@users.noreply.github.com>
2023-12-30 18:39:50 +0000

    refactor: move API Client for his own file

--- src/core.ts
+++ src/core.ts
@@ -1,13 +1,114 @@
+import axios from "axios";
+import { RequestError } from "@bunnyjs/errors";
+
 export const readEnv = (envName: string): string | undefined => {
-    if (typeof process === "undefined" || typeof process.env === "undefined") {
-      return undefined;
+  if (typeof process === "undefined" || typeof process.env === "undefined") {
+    return undefined;
+  }
+
+  const env = process.env[envName];
+
+  if (typeof env === "undefined") {
+    return undefined;
+  }
+
+  return env;
+};
+
+export class APIClient {
+  private baseUrl: string;
+  private accessKey: string;
+
+  constructor(params: APIClient.Params) {
+    this.baseUrl = params.baseUrl;
+    this.accessKey = params.accessKey;
+
+    if (!this.baseUrl) {
+      throw new Error("No BASE_URL was provided");
     }
-  
-    const env = process.env[envName];
-  
-    if (typeof env === "undefined") {
-      return undefined;
+
+    if (!this.accessKey) {
+      throw new Error("No ACCESS_KEY was provided");
     }
-  
-    return env;
+  }
+
+  async get(endpoint: string, input?: APIClient.Request): Promise<any> {
+    return this.makeRequest(endpoint, "GET", input);
+  }
+
+  private async makeRequest(
+    endpoint: string,
+    method: APIClient.Methods,
+    input?: APIClient.Request
+  ): Promise<APIClient.Response> {
+    const url = this.buildURL(endpoint);
+    const options = this.buildOptions(input);
+
+    try {
+      const response = await axios.request({
+        url,
+        method,
+        ...options,
+      });
+
+      if (response.status >= 400) {
+        const errorMessage =
+          response.data.title ?? response.data.Message ?? "Request failed";
+        const error = new RequestError(errorMessage, response.status);
+        throw error;
+      }
+
+      return {
+        status: "success",
+        statusCode: response.status,
+        data: response.data,
+      };
+    } catch (error: any) {
+      const status = error.status ?? 400;
+      const errorMessage = error.message ?? "Request failed";
+
+      return {
+        status: "failure",
+        statusCode: status,
+        data: {
+          error: errorMessage,
+        },
+      };
+    }
+  }
+  private buildURL(endpoint: string) {
+    return `${this.baseUrl}${endpoint}`;
+  }
+
+  private buildOptions(input?: APIClient.Request) {
+    const options = {
+      body: input?.body,
+      headers: {
+        ...input?.headers,
+        AccessKey: this.accessKey,
+      },
+    };
+
+    return options;
+  }
+}
+
+export namespace APIClient {
+  export type Params = {
+    baseUrl: string;
+    accessKey: string;
   };
+
+  export type Request = {
+    body?: any;
+    headers?: any;
+  };
+
+  export type Response = {
+    status: "success" | "failure";
+    statusCode: number;
+    data: any;
+  };
+
+  export type Methods = "GET" | "POST" | "PUT" | "DELETE";
+}
