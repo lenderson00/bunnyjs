e1b110d7b13b13ffce0086757017fb0f684591c4
Lenderson Macedo <61628075+lenderson00@users.noreply.github.com>
2023-12-30 15:14:34 +0000

    test: should return env it it was found

--- tests/core.spec.ts
+++ tests/core.spec.ts
@@ -3,13 +3,29 @@ const readEnv = (envName: string): string | undefined => {
     return undefined;
   }
 
-  return undefined;
+  const env = process.env[envName];
+
+  if (typeof env === "undefined") {
+    return undefined;
+  }
+
+  return env;
 };
 
 describe("CoreTests", () => {
   describe("readEnv", () => {
+    const originalProcess = process.env;
+
+    beforeEach(() => {
+      jest.resetModules();
+      process.env = {};
+    });
+
+    afterEach(() => {
+      process.env = originalProcess;
+    });
     it("should return undefined if no ENV was found", () => {
-      const envName = "NODE_ENV";
+      const envName = "FAKE_ENV";
 
       const sut = readEnv(envName);
 
@@ -36,5 +52,16 @@ describe("CoreTests", () => {
 
       Object.defineProperty(global, "process", { value: originalProcess });
     });
+
+    it("should return the ENV value if it was found", () => {
+      const envName = "NODE_ENV";
+      const envValue = "development";
+
+      process.env[envName] = envValue;
+
+      const sut = readEnv(envName);
+
+      expect(sut).toBe(envValue);
+    })
   });
 });
