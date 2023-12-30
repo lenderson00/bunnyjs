14a287b75a532934c5341a349befe0743c60f847
Lenderson Macedo <61628075+lenderson00@users.noreply.github.com>
2023-12-30 15:10:51 +0000

    test: ensure return undefiend in readEnv if no process was found

--- tests/core.spec.ts
+++ tests/core.spec.ts
@@ -1,4 +1,8 @@
 const readEnv = (envName: string): string | undefined => {
+  if (typeof process === "undefined" || typeof process.env === "undefined") {
+    return undefined;
+  }
+
   return undefined;
 };
 
@@ -10,6 +14,27 @@ describe("CoreTests", () => {
       const sut = readEnv(envName);
 
       expect(sut).toBeUndefined();
+    });
+
+    it("should return undefined if no process was found", () => {
+      const originalProcess = global.process;
+
+      Object.defineProperty(global, "process", {
+        value: undefined,
+        writable: true,
+      });
+
+      const envName = "NODE_ENV";
+
+      jest.mock("process", () => {
+        env: undefined;
+      });
+
+      const sut = readEnv(envName);
+
+      expect(sut).toBeUndefined();
+
+      Object.defineProperty(global, "process", { value: originalProcess });
     });
   });
 });
